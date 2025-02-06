50fcc314b11eaeef13cf4ef1856dbe6e35a32d5a
MalachiB04 <butler.malachi2018@gmail.com>
2025-02-06 12:49:58 -0500

    Update workingWithshapes.py

--- workingWithshapes.py
+++ workingWithshapes.py
@@ -1,33 +1,25 @@
 '''
-File Name: workingWithshapes.py
-Author: Malachi Butler 
+File Name: workingWithshapes v.2.py;
     
- Purpose: Calculates area of squares and triangles. Demonstrates loops by
- restricting user input to proper format (integer or float in this case)
-    
-Requirements: Python 3.1 
+ Purpose: Calculates area of squares and cubes and triangles! User is able to enter in unique values, and 
+ choose amongst the available options. Loops to ensure valid inputs. 
+ 
+Requierments: Python 3.1;
+ 
+ File Created: 02/03, 2025;
+ Last Updated: 02/04, 2025;  
+ Author: Malachi Butler;   
+
 Version 3.1
 '''
-import math
 #area of a square
-print("Area of Your Square:")
 def square():
-    length = input("Lengh: ")
-    width = input("Width: ")
+    length = int(input("Lengh for Square: "));
+    width = int(input("Width for Square: "));
     areaOne = float(length) * float(width)
-    return areaOne
-
-#loop to ensure integer
-while True: 
-    try:
-        print("The Area of the Square is:",  str(square()))
-        break
-    except ValueError: 
-        print("Try Again :) ")
-    continue
+    return areaOne;
 
 #area of a cube
-print("\nArea of your Cube:")
 def cube(): 
     lengthTwo = input("Lengh for Cube: ")
     widthTwo = input("Width for Cube: ")
@@ -35,14 +27,34 @@ def cube():
     areaTwo = float(lengthTwo) * float(widthTwo) * float(heightOne)
     return areaTwo 
 
-#loop to ensure integer
-while True: 
+#area of a triangle
+def triangle(): 
+    lenghtThree = input("Lenght for the Triangle: ")
+    widthThree = input("Width for the Triangle: ")
+    areaThree = 0.5 * float(widthThree) * float(lenghtThree)
+    return areaThree
+
+userInputone = "Square"
+userInputtwo = "Cube"
+userInputThree = "Triangle"
+
+print("** MAKE SURE TO CAPATALIZE FIRST LETTER!! **")
+userInput = str(input("Calculate the area of a Square, Cube, or Triangle? "))
+
+while True:
     try:
-        print("The Area of the Cube is:", str(cube()))
-        break
+        if userInput == userInputone: 
+            print("The Area of your Square is: " + str(square()))
+            break
+        elif userInput == userInputtwo:
+            print("The Area of your Cube is: " + str(cube()))        
+            break
+        elif userInput == userInputThree: 
+            print("The Area of your Triangle is: " + str(triangle()))
+            break
+        else: 
+            print("Make sure to capatalize first letter or enter out of three options! ")
+            userInput = str(input("Calculate the area of a Square, Cube, or Triangle? "))
     except ValueError: 
-        print("Try Again :) ")
-    continue
-
-
-
+        print("Oops! Looks like your finger slipped! Try Again :)")
+        continue
